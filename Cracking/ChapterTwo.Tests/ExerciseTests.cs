e0193d993d939587e36d94623ef1faa6f2bfcbcf
Ivan Matantsev <ivmatan@microsoft.com>
2020-09-01 21:50:18 -0700

    Add chapter two (last exercise is missing

--- Cracking/ChapterTwo.Tests/ExerciseTests.cs
+++ Cracking/ChapterTwo.Tests/ExerciseTests.cs
@@ -22,7 +22,7 @@ namespace Cracking.ChapterTwo.Tests
             var head = Node.CreateFromArray(new int[] { 1, 1, 1, 2, 2 });
             Exercises.RemoveDuplicates(head);
             Assert.Equal(new int[] { 1, 2 }, Node.GetContent(head));
-            
+
             head = Node.CreateFromArray(new int[] { 1, 2, 3, 4 });
             Exercises.RemoveDuplicates(head);
             Assert.Equal(new int[] { 1, 2, 3, 4 }, Node.GetContent(head));
@@ -35,6 +35,53 @@ namespace Cracking.ChapterTwo.Tests
             head = Node.CreateFromArray(new int[] { 1, 2, 3, 4 });
             Exercises.RemoveDuplicatesNoExtraMemory(head);
             Assert.Equal(new int[] { 1, 2, 3, 4 }, Node.GetContent(head));
+        }
+
+        [Fact]
+        public void GetNthElementFromEnd()
+        {
+            var head = Node.CreateFromArray(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+            var result = Exercises.GetNthElementFromEnd(head, 3);
+
+            Assert.Equal(5, result.Item);
+            result = Exercises.GetNthElementFromEnd(head, 2);
+            Assert.Equal(6, result.Item);
+            result = Exercises.GetNthElementFromEnd(head, 1);
+            Assert.Equal(7, result.Item);
+            result = Exercises.GetNthElementFromEnd(head, 10);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void DeleteNode()
+        {
+            var head = Node.CreateFromArray(new int[] { 1, 2, 3, 4 });
+            var node = head.Next.Next;
+            Exercises.DeleteNode(node);
+            Assert.Equal(new int[] { 1, 2, 4 }, Node.GetContent(head));
+        }
+
+        [Fact]
+        public void Summarize()
+        {
+            var headOne = Node.CreateFromArray(new int[] { 3, 2, 1 });
+            var headTwo = Node.CreateFromArray(new int[] { 3, 2, 1 });
+            var sum = Exercises.Summarize(headOne, headTwo);
+            Assert.Equal(new int[] { 6, 4, 2 }, Node.GetContent(sum));
+            headOne = Node.CreateFromArray(new int[] { 9, 9, 9 });
+            headTwo = Node.CreateFromArray(new int[] { 9, 9, 9 });
+            sum = Exercises.Summarize(headOne, headTwo);
+            Assert.Equal(new int[] { 8, 9, 9, 1 }, Node.GetContent(sum));
+
+            headOne = Node.CreateFromArray(new int[] { 1 });
+            headTwo = Node.CreateFromArray(new int[] { 9, 9, 9 });
+            sum = Exercises.Summarize(headOne, headTwo);
+            Assert.Equal(new int[] { 0, 0, 0, 1 }, Node.GetContent(sum));
+
+            headOne = Node.CreateFromArray(new int[] { 9, 9, 9 });
+            headTwo = Node.CreateFromArray(new int[] { 9 });
+            sum = Exercises.Summarize(headOne, headTwo);
+            Assert.Equal(new int[] { 8, 0, 0, 1 }, Node.GetContent(sum));
         }
     }
 }
--- Cracking/ChapterTwo/Exercises.cs
+++ Cracking/ChapterTwo/Exercises.cs
@@ -53,5 +53,84 @@ namespace Cracking.ChapterTwo
                 head = head.Next;
             }
         }
+        /// <summary>
+        /// Implement an algorithm to find the nth to last element of a singly linked list.
+        /// </summary>
+        public static Node GetNthElementFromEnd(Node head, int n)
+        {
+            if (n < 1 || head == null)
+                return null;
+            var orignal = head;
+            var current = head;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (current == null)
+                    return null;
+                current = current.Next;
+            }
+            while (current.Next != null)
+            {
+                current = current.Next;
+                orignal = orignal.Next;
+            }
+            return orignal;
+        }
+
+        /// <summary>
+        /// Implement an algorithm to delete a node in the middle of a single linked list, given
+        /// only access to that node.
+        /// </summary>
+        public static void DeleteNode(Node node)
+        {
+            if (node == null || node.Next == null)
+                return;
+            node.Item = node.Next.Item;
+            node.Next = node.Next.Next;
+        }
+
+        /// <summary>
+        /// You have two numbers represented by a linked list, where each node contains a sin-
+        /// gle digit.The digits are stored in reverse order, such that the 1’s digit is at the head of
+        /// the list. Write a function that adds the two numbers and returns the sum as a linked
+        /// list.
+        /// </summary>
+        public static Node Summarize(Node headOne, Node headTwo)
+        {
+            var head = new Node();
+            var last = head;
+            int carryOver = 0;
+            while (headOne != null && headTwo != null)
+            {
+                var current = new Node() { Item = (headOne.Item + headTwo.Item + carryOver) % 10 };
+                last.Next = current;
+                last = last.Next;
+                carryOver = (headOne.Item + headTwo.Item + carryOver) / 10;
+                headOne = headOne.Next;
+                headTwo = headTwo.Next;
+            }
+            while (headOne != null)
+            {
+                var current = new Node() { Item = (headOne.Item + carryOver) % 10 };
+                last.Next = current;
+                last = last.Next;
+                carryOver = (headOne.Item + carryOver) / 10;
+                headOne = headOne.Next;
+            }
+            while (headTwo != null)
+            {
+                var current = new Node() { Item = (headTwo.Item + carryOver) % 10 };
+                last.Next = current;
+                last = last.Next;
+                carryOver = (headTwo.Item + carryOver) / 10;
+                headTwo = headTwo.Next;
+            }
+            if (carryOver != 0)
+            {
+                var current = new Node() { Item = carryOver };
+                last.Next = current;
+                last = last.Next;
+            }
+            return head.Next;
+        }
     }
 }
